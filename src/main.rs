8a025e9a47b917fa0f73ac5891bdc280bbe73163
William Lehman <willyp@Williams-MacBook-Pro.local>
2021-09-07 19:54:23 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let name = "uniform distribution in Rust";
+    let u= UniformDistribution{min: 1.0, max :2.0};
+    println!("{}, {}!", name, u.inv_cdf(0.25));
+}
+
+trait DistributedVariable{
+    fn inv_cdf(&self, probability: f64) -> f64;
+}
+struct UniformDistribution{
+    min: f64,
+    max: f64
+}
+impl DistributedVariable for UniformDistribution{
+    fn inv_cdf(&self, probability: f64) -> f64{
+        self.min + ((self.max - self.min) * probability)
+    }
+}
