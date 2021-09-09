70b53b790273ba4b25662e9c8323373a04b71dfd
HenryGeorgist <william.p.lehman@usace.army.mil>
2021-09-09 15:27:56 -0500

    wip, cannot figure out how to allocate a slice

--- src/paireddata/uncertainpaireddata.rs
+++ src/paireddata/uncertainpaireddata.rs
@@ -1,14 +1,19 @@
 use super::PairedData;
-
+use statistics::DistributedVariable;
 pub trait PairedDataSampler{
     fn sample(&self, randomvalue: f64) -> PairedData;
 }
 pub struct UncertainPairedData<'a>{
     pub xvals : &'a[f64],
-    pub yvals : &'a[f64]//dyn DistributedVariable]
+    pub yvals : Vec<&'a dyn DistributedVariable>
 }
 impl PairedDataSampler for UncertainPairedData<'_>{
     fn sample(&self, randomvalue: f64)-> PairedData{
-        PairedData{xvals: self.xvals, yvals: self.yvals}
+        let size = self.xvals.len();
+        let ys : [f64;size] = [0.0;size];//how do i allocate a slice?
+        for i in 0..size{
+            ys[i] = self.yvals[i].inv_cdf(randomvalue);
+        }
+        PairedData{xvals: self.xvals, yvals: ys}
     }
 }
